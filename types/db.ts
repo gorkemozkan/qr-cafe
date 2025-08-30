54268e549b98cf98782855618262a76cccdc7e78
gorkem <pities-yuppies0v@icloud.com>
2025-08-30 16:57:12 +0300

    Update Supabase migration scripts to create new 'testo' table and adjust permissions; modify package.json for migration command naming consistency.

--- types/db.ts
+++ types/db.ts
@@ -79,6 +79,21 @@ export type Database = {
         }
         Relationships: []
       }
+      testo: {
+        Row: {
+          created_at: string
+          id: number
+        }
+        Insert: {
+          created_at?: string
+          id?: number
+        }
+        Update: {
+          created_at?: string
+          id?: number
+        }
+        Relationships: []
+      }
     }
     Views: {
       [_ in never]: never
