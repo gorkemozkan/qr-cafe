cc33b33c378fccb88f492bd16dff080e9dc2fddb
gorkem <pities-yuppies0v@icloud.com>
2025-09-08 12:55:09 +0300

    Refactor menu page component to simplify structure and improve readability
    - Removed unnecessary metadata generation and API calls, streamlining the page component.
    - Updated the component to a functional format, returning a simple placeholder for improved clarity.

--- app/(menu)/[slug]/page.tsx
+++ app/(menu)/[slug]/page.tsx
@@ -1,50 +1,7 @@
-import { NextPage } from "next";
-import { notFound } from "next/navigation";
-import SimpleMenu from "@/components/menu/SimpleMenu/SimpleMenu";
-import { publicMenuRepository } from "@/lib/repositories/public-menu-repository";
+import React from "react";
 
-interface Params {
-  params: Promise<{ slug: string }>;
-}
-export const generateMetadata = async ({ params }: Params) => {
-  const { slug } = await params;
-  console.log("slug", slug);
-  if (!slug) {
-    return {
-      title: "Menu",
-      description: "Menu",
-    };
-  }
-
-  console.log("slug", slug);
-
-  const menu = await publicMenuRepository.getMenuBySlug(slug);
-
-  console.log("menu", menu);
-
-  return {
-    title: menu?.cafe.name,
-    description: menu?.cafe.description,
-  };
+const page = () => {
+  return <div>page</div>;
 };
 
-const Page: NextPage<Params> = async (props) => {
-  const { slug } = await props.params;
-
-  console.log("slug", slug);
-  if (!slug) {
-    notFound();
-  }
-
-  const menu = await publicMenuRepository.getMenuBySlug(slug);
-
-  console.log("menu", menu);
-
-  if (!menu) {
-    notFound();
-  }
-
-  return null;
-};
-
-export default Page;
+export default page;
