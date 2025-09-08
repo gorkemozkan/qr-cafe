7cdaa5114406821224f03688df1f8f9bdd7cd84d
gorkem <pities-yuppies0v@icloud.com>
2025-09-08 12:58:42 +0300

    Refactor menu page component to utilize Next.js features and enhance data fetching
    - Updated the menu page component to use NextPage from Next.js for better type safety.
    - Implemented asynchronous data fetching for the menu based on the slug parameter.
    - Added console logs for debugging slug and menu data retrieval, improving traceability.

--- app/(menu)/[slug]/page.tsx
+++ app/(menu)/[slug]/page.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { NextPage } from "next";
+import { publicMenuRepository } from "@/lib/repositories/public-menu-repository";
 
-const page = () => {
-  return <div>page</div>;
+interface Params {
+  params: Promise<{ slug: string }>;
+}
+
+const Page: NextPage<Params> = async (props) => {
+  const { slug } = await props.params;
+
+  console.log("slug", slug);
+
+  const menu = await publicMenuRepository.getMenuBySlug(slug);
+
+  console.log("menu", menu);
+
+  return <div>menu</div>;
 };
 
-export default page;
+export default Page;
