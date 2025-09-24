3f72cf93273d0fa3c44883c8dbd638a61e96c018
gorkem <pities-yuppies0v@icloud.com>
2025-09-24 16:43:31 +0300

    Update remote schema migration to create index only if it does not exist
    - Modified the SQL migration to create the index on cafes.user_id with the IF NOT EXISTS clause, ensuring that the index is only created if it doesn't already exist, improving migration reliability.

--- supabase/migrations/20250924130740_remote_schema.sql
+++ supabase/migrations/20250924130740_remote_schema.sql
@@ -10,10 +10,9 @@ drop policy "Enable update for users based on user_id" on "public"."categories";
 
 drop policy "Enable update for users based on user_id" on "public"."products";
 
-CREATE INDEX cafes_user_id_idx ON public.cafes USING btree (user_id);
+CREATE INDEX IF NOT EXISTS cafes_user_id_idx ON public.cafes USING btree (user_id);
 
 CREATE INDEX idx_categories_cafe_id ON public.categories USING btree (cafe_id);
-
 
   create policy "Delete own cafes"
   on "public"."cafes"
