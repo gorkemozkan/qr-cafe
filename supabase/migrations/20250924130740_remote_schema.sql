87b828c6a3b1b7accb3b5949fd2339280f45f7a7
gorkem <pities-yuppies0v@icloud.com>
2025-09-24 16:42:03 +0300

    Refactor storage bucket creation in SQL migrations to use storage.create_bucket for compatibility with the latest schema
    - Updated the creation of storage buckets for cafe logos, product images, and category images to utilize the storage.create_bucket function.
    - Added exception handling to ignore errors if the bucket already exists.
    - Removed legacy INSERT statements for improved maintainability.

---
+++ supabase/migrations/20250924130740_remote_schema.sql
@@ -0,0 +1,83 @@
+drop policy "Enable delete for users based on user_id" on "public"."cafes";
+
+drop policy "Enable insert for authenticated users only" on "public"."cafes";
+
+drop policy "Enable update for users based on user_id" on "public"."cafes";
+
+drop policy "Enable users to view their own data only" on "public"."cafes";
+
+drop policy "Enable update for users based on user_id" on "public"."categories";
+
+drop policy "Enable update for users based on user_id" on "public"."products";
+
+CREATE INDEX cafes_user_id_idx ON public.cafes USING btree (user_id);
+
+CREATE INDEX idx_categories_cafe_id ON public.categories USING btree (cafe_id);
+
+
+  create policy "Delete own cafes"
+  on "public"."cafes"
+  as permissive
+  for delete
+  to authenticated
+using ((( SELECT auth.uid() AS uid) = user_id));
+
+
+
+  create policy "Select own cafes"
+  on "public"."cafes"
+  as permissive
+  for select
+  to authenticated
+using ((( SELECT auth.uid() AS uid) = user_id));
+
+
+
+  create policy "Enable insert for authenticated users only"
+  on "public"."cafes"
+  as permissive
+  for insert
+  to authenticated
+with check ((( SELECT auth.uid() AS uid) = user_id));
+
+
+
+  create policy "Enable update for users based on user_id"
+  on "public"."cafes"
+  as permissive
+  for update
+  to authenticated
+using ((( SELECT auth.uid() AS uid) = user_id))
+with check ((( SELECT auth.uid() AS uid) = user_id));
+
+
+
+  create policy "Enable users to view their own data only"
+  on "public"."cafes"
+  as permissive
+  for select
+  to authenticated
+using ((( SELECT auth.uid() AS uid) = user_id));
+
+
+
+  create policy "Enable update for users based on user_id"
+  on "public"."categories"
+  as permissive
+  for update
+  to authenticated
+using ((( SELECT auth.uid() AS uid) = user_id))
+with check ((( SELECT auth.uid() AS uid) = user_id));
+
+
+
+  create policy "Enable update for users based on user_id"
+  on "public"."products"
+  as permissive
+  for update
+  to authenticated
+using ((( SELECT auth.uid() AS uid) = user_id))
+with check ((( SELECT auth.uid() AS uid) = user_id));
+
+
+
